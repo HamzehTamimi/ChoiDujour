1b7222dd19da56a97a5184feca42d71845287615
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-05-04 10:09:02 +0200

    Initial commit
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+val versionMajor = 0
+val versionMinor = 0
+val versionPatch = 1
+
+android {
+    namespace = "io.mesalabs.choidujour"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "io.mesalabs.choidujour"
+        minSdk = 30
+        targetSdk = 33
+        versionCode = versionMajor * 100000 + versionMinor * 1000 + versionPatch
+        versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+    }
+
+    lint {
+        disable += "AppCompatResource"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        release {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+configurations.all {
+    exclude(group = "androidx.appcompat", module = "appcompat")
+    exclude(group = "androidx.core", module = "core")
+    exclude(group = "androidx.customview", module = "customview")
+    exclude(group = "androidx.fragment", module = "fragment")
+}
+
+dependencies {
+    // Sesl: https://github.com/OneUIProject/oneui-core/tree/sesl4
+    implementation("io.github.oneuiproject.sesl:appcompat:1.3.0")
+    implementation("io.github.oneuiproject.sesl:material:1.4.0") {
+        exclude(group = "io.github.oneuiproject.sesl", module = "viewpager2")
+    }
+    // AndroidX: https://developer.android.com/jetpack/androidx/versions
+    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.viewpager2:viewpager2:1.0.0") {
+        exclude(group = "androidx.recyclerview", module = "recyclerview")
+    }
+}
